eeb8e3279d6e082135101ce7ae076c1c74b6b46d
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2025-10-11 11:40:48 +0530

    Feat:Complete

---
+++ script.js
@@ -0,0 +1,15 @@
+const button =document.querySelector('.share-icon');
+const socialLinks=document.querySelector('.social-links');
+
+button.addEventListener('click',()=>
+{
+    socialLinks.classList.toggle('active');
+    if(button.classList.contains('share-active'))
+    {
+        button.classList.remove('share-active','share-active-img');
+    }
+    else
+    {
+        button.classList.add('share-active','share-active-img');
+    }
+})
